ae10c95e6957a910b690e860706e0236f6627c2a
Andriy-Anti-Anime <savulaandrij84@gmail.com>
2025-09-02 18:31:25 +0300

    Add files via upload

---
+++ laba.py
@@ -0,0 +1,32 @@
+age = 1
+tasks_completed = 5
+coordinates = (1, 1)
+status = 'Work'
+quest_name = 'quest'
+range_values = (10, 12)
+score = 12
+time_interval = (1, 12)
+goal = 'Learn'
+level = 21
+
+print(age)
+print(tasks_completed)
+print(coordinates)
+print(status)
+print(quest_name)
+print(range_values)
+print(score)
+print(time_interval)
+print(goal)
+print(level)
+
+print(type(age))
+print(type(tasks_completed))
+print(type(coordinates))
+print(type(status))
+print(type(quest_name))
+print(type(range_values))
+print(type(score))
+print(type(time_interval))
+print(type(goal))
+print(type(level))
