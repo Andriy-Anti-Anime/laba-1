759cea775ce4d3f4f72b3af1c7b5fbd61052be97
Andriy-Anti-Anime <savulaandrij84@gmail.com>
2025-12-06 16:10:37 +0200

    Update laba 4.py

--- laba 4.py
+++ laba 4.py
@@ -9,6 +9,17 @@ store = {
 
 
 def convertator(price):
+store = {
+    "телефон": 9999.00,
+    "зубна щітка": 91.50,
+    "банан": 44.12,
+    "чай": 120.00,
+    "зошит": 31.30,
+    "ручка": 9.99
+}
+
+
+def convertator(price):
     return f'ціна: {price:.2f} грн'
 
 
@@ -40,15 +51,15 @@ def check_order(order, option):
     else:
         return "Ви вказали невідому опцію"
 
+if __name__ == "__main__":
+    print(convertator(100))
+    print(convertator(23.98763))
+    print(convertator(83282.2))
 
-print(convertator(100))
-print(convertator(23.98763))
-print(convertator(83282.2))
+    print(availability("хліб", "чай", "робот", "ручка"))
 
-print(availability("хліб", "чай", "робот", "ручка"))
+    print(check_order(["чай", "зубна щітка"], "price"))
 
-print(check_order(["чай", "зубна щітка"], "price"))
+    print(check_order(["телефон", "зубна щітка"], "buy"))
 
-print(check_order(["телефон", "зубна щітка"], "buy"))
-
-print(check_order(["телефон", "кетчуп"], "buy"))
+    print(check_order(["телефон", "кетчуп"], "buy"))
